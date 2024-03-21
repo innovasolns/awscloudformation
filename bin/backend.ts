d23ecfcb54fa6833e19f3d4c7157773024e1f5a7
Jerome Raymond <jerome.raymond@cityelectricsupply.com>
2024-03-20 21:33:55 -0500

    initial commit

--- bin/backend.ts
+++ bin/backend.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AwsStack } from '../lib/aws-stack';
+import { AwsStack } from '../stacks/aws-stack';
 
 const app = new cdk.App();
 new AwsStack(app, 'AwsStack', {
